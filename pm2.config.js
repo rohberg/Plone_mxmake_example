689c2e922df1971b318ef14a70bf3abdea1bc1cf
ksuess <k.suess@rohberg.ch>
2022-06-23 14:02:32 +0200

    Create pm2.config.js

---
+++ pm2.config.js
@@ -0,0 +1,14 @@
+let apps = [
+    {
+      name   : "plone_backend_tutorial",
+      script: 'runwsgi instance/etc/zope.ini',
+      cwd: 'backend'
+    },
+    // {
+    //   name   : "plone_frontend_tutorial",
+    //   script: 'yarn build && yarn start:prod',
+    //   cwd: 'frontend'
+    // }
+  ];
+
+module.exports = { apps: apps };
